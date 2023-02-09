b24119eeb6ac604c25efc982e396128b3ae8a74f
Jefferson Pedro dos Santos Silva <jeffersonpedro300@gmail.com>
2023-02-09 13:15:00 -0300

    ready

---
+++ M1S01/savecollege.js
@@ -0,0 +1,38 @@
+const students = [
+    {
+      name: "Jose",
+      height: 1.6,
+      age: 25,
+      sex: "M",
+    },
+
+    {
+      name: "Maria",
+      height: 1.65,
+      age: 26,
+      sex: "F"
+    },
+
+    {
+      name: "João",
+      height: 1.7,
+      age: 30,
+      sex: "M"
+    },
+
+    {
+      name: "Antonio",
+      height: 1.69,
+      age: 33,
+      sex: "M"
+    },
+
+    {
+      name: "Manoel",
+      height: 1.65,
+      age: 25,
+      sex: "M"
+    },
+  ];
+
+  console.log(students);
