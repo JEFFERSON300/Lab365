6be55bac005134307f28639403912859a9a9351c
Jefferson Pedro dos Santos Silva <jeffersonpedro300@gmail.com>
2023-02-09 13:54:13 -0300

    modify name file

--- Exercises/M1S01/savecollege.js
+++ Exercises/M1S01/savecollege.js
@@ -4,35 +4,61 @@ const students = [
       height: 1.6,
       age: 25,
       sex: "M",
+      classRoom: 2,
+      mathematics: 6
     },
 
     {
       name: "Maria",
       height: 1.65,
       age: 26,
-      sex: "F"
+      sex: "F",
+      classRoom: 1,
+      mathematics: 7.5
     },
 
     {
       name: "João",
       height: 1.7,
       age: 30,
-      sex: "M"
+      sex: "M",
+      classRoom: 2,
+      mathematics: 9.4
     },
 
     {
       name: "Antonio",
       height: 1.69,
       age: 33,
-      sex: "M"
+      sex: "M",
+      classRoom: 2,
+      mathematics: 10
     },
 
     {
       name: "Manoel",
       height: 1.65,
       age: 25,
-      sex: "M"
+      sex: "M",
+      classRoom: 1,
+      mathematics: 4.6
     },
   ];
 
   console.log(students);
+
+  let totalMathematics = 0;
+
+  totalMathematics = students[0].mathematics + students[1].mathematics + students[2].mathematics + students[3].mathematics + students[4].mathematics;
+  console.log(`A média da turma é ${totalMathematics/5}`);
+
+students.forEach(element => {
+  if (element.mathematics>5){
+    console.log(`O aluno ${element.name} foi aprovado`)
+  }
+  else{
+    console.log(`O aluno ${element.name} foi reprovado`)
+  }
+  
+});
+
