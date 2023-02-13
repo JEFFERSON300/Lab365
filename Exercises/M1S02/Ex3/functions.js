30338473b4499a39ecb7f4ac6e09d3a4e0233c16
JEFFERSON300 <jeffersonpedro300@gmail.com>
2023-02-13 16:01:53 -0300

    "Initial Version"

---
+++ Exercises/M1S02/Ex3/functions.js
@@ -0,0 +1,22 @@
+document.getElementById("dataForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const inputLogin = document.getElementById("login").value;
+  const inputPassword = document.getElementById("password").value;
+
+  if (dataUser.login == inputLogin && dataUser.password == inputPassword) {
+    console.log("Logado");
+
+    localStorage.setItem("login", String(inputLogin));
+    localStorage.setItem("password", String(inputPassword));
+  } else if (inputLogin == "" || inputPassword == "") {
+    console.log("Algum dos campos está vazio");
+  } else {
+    console.log("Credenciais incorretas");
+  }
+});
+
+let dataUser = {
+  login: "Jefferson",
+  password: 123456,
+};
