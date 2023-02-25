df434ac4dfeebfc51816d080f6affa994e19b6b4
JEFFERSON300 <jeffersonpedro300@gmail.com>
2023-02-25 09:50:56 -0300

    First Version

---
+++ Exercises/M1S04/Ex2/functions.js
@@ -0,0 +1,48 @@
+class Conta {
+  #senha;
+
+  constructor(saldo, senha) {
+    this.saldo = saldo;
+    this.#senha = senha;
+  }
+
+  get senha() {
+    return this.#senha;
+  }
+
+  set senha(novaSenha) {
+    this.#senha = novaSenha;
+  }
+
+  deposito(senhaDigitada) {
+    if (this.senha == senhaDigitada) {
+      let adicionar = Number(
+        window.prompt("Digite o valor que deseja adicionar ao seu saldo")
+      );
+      this.saldo += adicionar;
+      console.log(`Saldo final: ${this.saldo}`);
+    } else {
+      console.log("Senha invalida");
+    }
+  }
+
+  retirada(senhaDigitada) {
+    if (this.senha == senhaDigitada) {
+      let retirar = Number(
+        window.prompt("Digite o valor que deseja retirar do seu saldo")
+      );
+      if (this.saldo < retirar) {
+        console.log("Saldo Insuficiente");
+      } else {
+        this.saldo -= retirar;
+        console.log(`Saldo final: ${this.saldo}`);
+      }
+    } else {
+      console.log("Senha invalida");
+    }
+  }
+}
+
+let contaCorrente = new Conta(300, 1235);
+contaCorrente.deposito(1235);
+contaCorrente.retirada(1235);
