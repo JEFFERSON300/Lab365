1670400f517ad4f8830cc3a06ea6d862ac6f30c1
JEFFERSON300 <jeffersonpedro300@gmail.com>
2023-06-04 10:45:35 -0300

    Update

--- Exercises/M2S04/Ex1/project1/src/App.js
+++ Exercises/M2S04/Ex1/project1/src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
+      <body className="App-header">
         <p>Hello World!</p>
-      </header>
+      </body>
     </div>
   );
 }
---
+++ Exercises/M2S04/Ex1/project1/src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Header() {
+  const name = "Jefferson";
+  return <h1>{name}</h1>;
+}
+
+export default Header;
