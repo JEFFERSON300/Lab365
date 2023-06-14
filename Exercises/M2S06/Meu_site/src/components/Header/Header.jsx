a817a921a3d6abb1f81c67ecfd2d69254bc7ef4f
Jefferson Pedro dos Santos Silva <jeffersonpedro300@gmail.com>
2023-06-14 13:24:14 -0300

    add Component Header

---
+++ Exercises/M2S06/Meu_site/src/components/Header/Header.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Header.css";
+import PropTypes from "prop-types";
+
+function Header(props) {
+  
+  const menuItem = (item) => {
+    return (
+      <li className="nav-item">
+        <a
+          className="nav-link active text-light fw-bold me-3 fs-6"
+        >
+          {item.label}
+        </a>
+      </li>
+    );
+  };
+
+  const menuEmpty = () => {
+    return <p>Nada</p>;
+  };
+
+  return (
+    <nav style={{ height: 100 }}>
+      <ul className="nav d-flex flex-wrap align-items-center bg-dark w-100 h-100 ">
+        <li className="nav-item me-auto ms-1.25">
+          <a className="nav-link active text-light fw-bold fs-2" href="#">
+            NomeLoja
+          </a>
+        </li>
+        {props.menu.length > 0 ? props.menu.map(menuItem) : menuEmpty()}
+      </ul>
+    </nav>
+  );
+}
+
+Header.propTypes = {
+  menu: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      label: PropTypes.string.isRequired,
+      path: PropTypes.string,
+    })
+  ),
+};
+
+export default Header;
