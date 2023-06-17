511399c804cef3d6d6a5f1eef3e3ea32477657a7
JEFFERSON300 <jeffersonpedro300@gmail.com>
2023-06-17 08:41:20 -0300

    add Banner

--- Exercises/M2S06/Meu_site/src/App.jsx
+++ Exercises/M2S06/Meu_site/src/App.jsx
@@ -1,6 +1,6 @@
-import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Banner from "./components/Banner/Banner";
 
 function App() {
   const menu = [
@@ -15,6 +15,7 @@ function App() {
   return (
     <div>
       <Header menu={menu} />
+      <Banner />
     </div>
   );
 }
